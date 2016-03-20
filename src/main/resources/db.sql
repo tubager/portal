4e989f019edda8de22cb5e187f8ac1abc0be6419
Eric Shi <eric.shi@convertlab.com>
2016-03-20 11:12:23 +0800

    register

---
+++ src/main/resources/db.sql
@@ -0,0 +1,217 @@
+CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `test`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+--
+-- Host: 127.0.0.1    Database: test
+-- ------------------------------------------------------
+-- Server version	5.6.24
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article` (
+  `uuid` varchar(32) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `cover_img` varchar(128) DEFAULT NULL,
+  `user_name` varchar(45) DEFAULT NULL,
+  `location_name` varchar(45) DEFAULT NULL,
+  `status` varchar(1) NOT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `finish_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`uuid`,`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Article';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item` (
+  `uuid` varchar(32) NOT NULL,
+  `book_uuid` varchar(32) DEFAULT NULL,
+  `status` varchar(1) DEFAULT NULL,
+  `index` int(11) DEFAULT NULL,
+  `type` varchar(15) DEFAULT NULL,
+  `text` varchar(4096) DEFAULT NULL,
+  `src` varchar(128) DEFAULT NULL,
+  `location_id` bigint(20) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Article item';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `auth`
+--
+
+DROP TABLE IF EXISTS `auth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `auth` (
+  `user_name` varchar(32) NOT NULL,
+  `password` varchar(128) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `last_updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Auth';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book` (
+  `uuid` varchar(32) NOT NULL,
+  `title` varchar(256) DEFAULT NULL,
+  `description` varchar(256) DEFAULT NULL,
+  `cover_img` varchar(45) DEFAULT NULL,
+  `user_name` varchar(45) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `finish_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Book';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `location`
+--
+
+DROP TABLE IF EXISTS `location`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `location` (
+  `uuid` varchar(32) NOT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `country` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='location';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `paragraph`
+--
+
+DROP TABLE IF EXISTS `paragraph`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `paragraph` (
+  `uuid` varchar(32) NOT NULL,
+  `title` varchar(128) DEFAULT NULL,
+  `index` int(11) DEFAULT NULL,
+  `book_uuid` varchar(32) DEFAULT NULL,
+  `text` varchar(4096) DEFAULT NULL,
+  `img` varchar(1024) DEFAULT NULL,
+  `img_text` varchar(1024) DEFAULT NULL,
+  `video` varchar(45) DEFAULT NULL,
+  `audio` varchar(45) DEFAULT NULL,
+  `location_uuid` varchar(45) DEFAULT NULL,
+  `timestamp` varchar(32) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `user_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Paragraph';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `token`
+--
+
+DROP TABLE IF EXISTS `token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `token` (
+  `token` varchar(45) NOT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `device` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Token';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `trace_spot`
+--
+
+DROP TABLE IF EXISTS `trace_spot`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trace_spot` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tech_id` varchar(32) DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `nick_name` varchar(45) DEFAULT NULL,
+  `gender` varchar(1) DEFAULT 'M',
+  `img` varchar(128) DEFAULT NULL,
+  `mobile` varchar(25) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `address` varchar(128) DEFAULT NULL,
+  `region` int(11) DEFAULT NULL,
+  `mobile_verified` varchar(1) DEFAULT NULL,
+  `email_verified` varchar(1) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `last_updated` datetime DEFAULT NULL,
+  `tech_id` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-11-18 13:57:29
